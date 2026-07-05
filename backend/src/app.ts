895260dd0fadf48252995038736df3638a41a4a0
Serge Basangovs <sergejs.basangovs@gmail.com>
2026-07-05 13:46:11 +0100

    feat: monorepo, backend, frontend, apps

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { router } from "@/routes/index";
 import { errorHandler } from "@/middleware/errorHandler";
 
 export const app = express();
 
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 app.use(router);
 app.use(errorHandler);
